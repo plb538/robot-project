6e6b86aa8781033fc6a4f5e1e3712917976a6b8e
Patrick Bonnell <plbonnell538@gmail.com>
2019-04-09 12:36:47 -0400

    Added USB controller input; quantized input; updated requirements
    Signed-off-by: Patrick Bonnell <plbonnell538@gmail.com>

--- robot_project/main.py
+++ robot_project/main.py
@@ -1,7 +1,31 @@
 #!/usr/bin/env python
 
 import RPi.GPIO as gpio
+from inputs import devices,get_gamepad
+import time
+
+
+def quantize(value, new_min=0, new_max=255, orig_min=0, orig_max=1023):
+    result = int(((float(value) - float(orig_min))*(float(new_max) - float(new_min)) / (float(orig_max) - float(orig_min)) + float(new_min)))
+    return result
+
+
 
 if __name__ == "__main__":
     print("Hello Boyz")
 
+    for device in devices:
+        print(device)
+
+    left_x = 0
+    left_y = 0
+
+    while 1:
+        events = get_gamepad()
+        for event in events:
+            if event.code == "ABS_X":
+                left_x = event.state
+            elif event.code == "ABS_Y":
+                left_y = event.state
+        print("Quantized X: {}".format(quantize(left_x, orig_min=-32768, orig_max=32768)))
+        print("Quantized Y: {}".format(quantize(left_y, orig_min=-32768, orig_max=32768))) 
