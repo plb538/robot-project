49c99ada00ff866daba1406f70f083b638d28fc4
Patrick Bonnell <plbonnell538@gmail.com>
2019-04-09 15:31:38 -0400

    Fooling around with PWM and USB controller input; made two LEDs change brightness using controller
    Signed-off-by: Patrick Bonnell <plbonnell538@gmail.com>

--- robot_project/main.py
+++ robot_project/main.py
@@ -5,17 +5,27 @@ from inputs import devices,get_gamepad
 import time
 
 
-def quantize(value, new_min=0, new_max=255, orig_min=0, orig_max=1023):
+def quantize(value, new_min=0, new_max=100, orig_min=0, orig_max=1023):
     result = int(((float(value) - float(orig_min))*(float(new_max) - float(new_min)) / (float(orig_max) - float(orig_min)) + float(new_min)))
     return result
 
 
+def setup():
+    gpio.setmode(gpio.BCM)
+    gpio.setup(23, gpio.OUT)
+    gpio.setup(24, gpio.OUT)
 
 if __name__ == "__main__":
     print("Hello Boyz")
-
+    setup()
     for device in devices:
         print(device)
+
+    plx = gpio.PWM(23, 100)
+    plx.start(0)
+
+    ply = gpio.PWM(24, 100)
+    ply.start(0)
 
     left_x = 0
     left_y = 0
@@ -27,5 +37,11 @@ if __name__ == "__main__":
                 left_x = event.state
             elif event.code == "ABS_Y":
                 left_y = event.state
+        q_left_x = quantize(left_x, orig_min=-32768, orig_max=32768)
+        q_left_y = quantize(left_y, orig_min=-32768, orig_max=32768)
         print("Quantized X: {}".format(quantize(left_x, orig_min=-32768, orig_max=32768)))
         print("Quantized Y: {}".format(quantize(left_y, orig_min=-32768, orig_max=32768))) 
+        
+        plx.ChangeDutyCycle(q_left_x)
+        ply.ChangeDutyCycle(q_left_y)
+
